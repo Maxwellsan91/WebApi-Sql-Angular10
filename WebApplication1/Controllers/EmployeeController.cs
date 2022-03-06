3fbfde00e9b0ce1f1d3343e4eedff48834bdc5ef
Maxwell Santos <maxwell.str@hotmail.com>
2022-03-06 18:45:58 +0000

    Adicionar arquivos de projeto.

---
+++ WebApplication1/Controllers/EmployeeController.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using WebApplication1.Models;
+
+
+namespace WebApplication1.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        public HttpResponseMessage Get()
+        {
+            string query = @"
+                            SELECT EmployeeId, EmployeeName, Department,
+                            convert(varchar(10),DateOfJoining,120) as DateOfJoining,
+                            PhotoFileName
+                            FROM [EmployeeDB].[dbo].[Employee]
+                    ";
+
+            DataTable table = new DataTable();
+
+            using (var con = new SqlConnection(ConfigurationManager.
+                ConnectionStrings["EmployeeAppDB"].ConnectionString))
+            using (var cmd = new SqlCommand(query, con))
+            using (var da = new SqlDataAdapter(cmd))
+            {
+                cmd.CommandType = CommandType.Text;
+                da.Fill(table);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, table);
+
+        }
+
+        public string Post(Employee emp)
+        {
+            try
+            {            
+                string query = @"
+                INSERT INTO[dbo].[Employee]                   
+                VALUES
+                    (
+                    '" + emp.EmployeeName + @"'
+                    ,'" + emp.Department + @"'
+                    ,'" + emp.DateOfJoining + @"'
+                    ,'" + emp.PhotoFileName + @"'
+                    )
+                ";
+
+                DataTable table = new DataTable();
+
+                using (var con = new SqlConnection(ConfigurationManager.
+                    ConnectionStrings["EmployeeAppDB"].ConnectionString))
+                using (var cmd = new SqlCommand(query, con))
+                using (var da = new SqlDataAdapter(cmd))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    da.Fill(table);
+                }
+
+                return "Added Successfully!";
+            }
+            catch (Exception)
+            {
+
+                return "Failed to Add!";
+            }
+        }
+
+        public string Put(Employee emp)
+        {
+            try
+            {
+                //string query = @"
+                //UPDATE[dbo].[Department]
+                //SET[DepartmentName]= '" + dep.DepartmentName + @"'
+                //WHERE DepartmentId=" + dep.DepartmentId + @"
+                //";
+                string query = @"
+                UPDATE[dbo].[Employee]
+                    SET
+                        [EmployeeName]='" + emp.EmployeeName + @"'
+                       ,[Department]='" + emp.Department + @"'
+                       ,[DateOfJoining]='" + emp.DateOfJoining + @"'
+                       ,[PhotoFileName]='" + emp.PhotoFileName + @"'
+                    WHERE EmployeeId=" + emp.EmployeeId + @"
+                ";
+
+                  DataTable table = new DataTable();
+
+                using (var con = new SqlConnection(ConfigurationManager.
+                    ConnectionStrings["EmployeeAppDB"].ConnectionString))
+                using (var cmd = new SqlCommand(query, con))
+                using (var da = new SqlDataAdapter(cmd))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    da.Fill(table);
+                }
+
+                return "Uppdate Successfully!";
+            }
+            catch (Exception)
+            {
+
+                return "Failed to Uppdate!";
+            }
+        }
+
+        public string Delete(int id)
+        {
+            try
+            {
+                string query = @"
+                                DELETE FROM [dbo].[Employee] 
+                                WHERE EmployeeId=" + id + @"        
+                             ";
+
+                DataTable table = new DataTable();
+
+                using (var con = new SqlConnection(ConfigurationManager.
+                    ConnectionStrings["EmployeeAppDB"].ConnectionString))
+                using (var cmd = new SqlCommand(query, con))
+                using (var da = new SqlDataAdapter(cmd))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    da.Fill(table);
+                }
+
+                return "Deleted Successfully!";
+            }
+            catch (Exception)
+            {
+
+                return "Failed to Delete!";
+            }
+        }
+
+        [Route("api/Employee/GetAllDepartmentNames")]
+        [HttpGet]
+        public HttpResponseMessage GetAllDepartmentNames()
+        {
+            string query = @"
+                               SELECT DepartmentName FROM [dbo].[Department] 
+                             ";
+
+
+            DataTable table = new DataTable();
+
+            using (var con = new SqlConnection(ConfigurationManager.
+                ConnectionStrings["EmployeeAppDB"].ConnectionString))
+            using (var cmd = new SqlCommand(query, con))
+            using (var da = new SqlDataAdapter(cmd))
+            {
+                cmd.CommandType = CommandType.Text;
+                da.Fill(table);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, table);
+        }
+
+        [Route("api/Employee/SaveFile")]
+        public string SaveFile()
+        {          
+            try
+            {
+                var httpRequest = HttpContext.Current.Request;
+                var postedFile = httpRequest.Files[0];
+                string filename = postedFile.FileName;
+                var physicalPath = HttpContext.Current.Server.MapPath("~/Models/Photos/"+filename);
+
+                postedFile.SaveAs(physicalPath);
+
+                return filename;
+            }
+            catch (Exception)
+            {
+
+                return "anonymous.png";
+            }
+
+        }
+    }
+}
